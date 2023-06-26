d635db12ff501b925f0f61708c29f939c7084c36
AnsariYasirArfat <ansariyaseer786@gmail.com>
2023-06-26 22:13:53 +0530

    Header, Outlets & Footer add in Root component App.js

--- dish-cuisine/src/App.jsx
+++ dish-cuisine/src/App.jsx
@@ -1,8 +1,20 @@
+import Header from "./Components/Header";
+import Footer from "./Components/Footer";
+import { useEffect } from "react";
+import { Outlet } from "react-router-dom";
+
 function App() {
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
   return (
     <>
       <div className="bg-white sm:bg-red-200 md:bg-green-300 lg:bg-yellow-200 xl:bg-blue-400 2xl:bg-teal-100">
-        Hello World!
+        <Header />
+        <div className="overflow-auto" style={{ height: "88vh" }}>
+          <Outlet />
+          <Footer />
+        </div>
       </div>
     </>
   );
