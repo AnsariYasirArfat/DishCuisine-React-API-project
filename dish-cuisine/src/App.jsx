e6fc3ca8e33315005c9a1f58c8cf898224a751e5
AnsariYasirArfat <ansariyaseer786@gmail.com>
2023-06-26 17:57:30 +0530

    Setup done &  all required package instaled & checked

--- dish-cuisine/src/App.jsx
+++ dish-cuisine/src/App.jsx
@@ -1,7 +1,9 @@
 function App() {
   return (
     <>
-      <h1 className="text-3xl font-bold underline">Hey! world!</h1>
+      <div className="bg-white sm:bg-red-200 md:bg-green-300 lg:bg-yellow-200 xl:bg-blue-400 2xl:bg-teal-100">
+        Hello World!
+      </div>
     </>
   );
 }
