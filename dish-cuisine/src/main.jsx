ebf28f203eaf6bbd42588de863229f8940dfe1be
AnsariYasirArfat <ansariyaseer786@gmail.com>
2023-06-26 17:51:24 +0530

    Router &  material ui installed

--- dish-cuisine/src/main.jsx
+++ dish-cuisine/src/main.jsx
@@ -1,10 +1,26 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import * as ReactDOM from "react-dom/client";
+import "./index.css";
+import { ThemeProvider } from "@material-tailwind/react";
+import {
+  createRoutesFromElements,
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+} from "react-router-dom";
+import App from "./App.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const MainSection = createBrowserRouter(
+  createRoutesFromElements(
+    <>
+      <Route path="/" element={<App />}></Route>
+    </>
+  )
+);
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <ThemeProvider>
+      <RouterProvider router={MainSection} />
+    </ThemeProvider>
+  </React.StrictMode>
+);
