8be9091affbd030ba51d512bee424b24783ad09d
AnsariYasirArfat <ansariyaseer786@gmail.com>
2023-06-27 22:47:24 +0530

    Random recipe api called & Loader component implement

--- dish-cuisine/src/Components/PopularRecipes.jsx
+++ dish-cuisine/src/Components/PopularRecipes.jsx
@@ -1,20 +1,32 @@
-import productImage from "../Assets/product.jpg";
+import { useEffect, useState } from "react";
+import axios from "axios";
 import RecipeCard from "./RecipeCard";
-function PopularProduct() {
-  const popularProducts = [
-    {
-      Image: productImage,
-    },
-    {
-      Image: productImage,
-    },
-    {
-      Image: productImage,
-    },
-    {
-      Image: productImage,
-    },
-  ];
+import RecipeCardLoader from "./RecipeCardLoader";
+
+function PopularRecipes() {
+  const [popularRecipes, setPopularRecipes] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  useEffect(() => {
+    async function getRandomRecipes() {
+      try {
+        const recipes = [];
+
+        for (let i = 0; i < 4; i++) {
+          const randomRecipe = await axios.get(
+            `https://www.themealdb.com/api/json/v1/1/random.php`
+          );
+          recipes.push(randomRecipe.data.meals[0]);
+        }
+
+        setPopularRecipes(recipes);
+        setIsLoading(false);
+      } catch (error) {
+        console.error("Error fetching recipes:", error);
+      }
+    }
+    getRandomRecipes();
+  }, []);
+
   return (
     <>
       <section
@@ -22,18 +34,20 @@ function PopularProduct() {
         className="my-8 mx-3 sm:mx-6 md:mx-8 lg:mx-5 p-5 bg-teal-300 rounded-lg"
       >
         <h1 className="pb-5 lg:pb-10 text-xl sm:text-2xl xl:text-3xl 2xl:text-4xl  font-bold text-center text-teal-50">
-          Popular Recipes
+          Random Recipes
         </h1>
         <div className="pb-8 flex flex-wrap justify-center gap-4 sm:gap-9 md:gap-3 lg:gap-x-3 xl:gap-x-12 2xl:gap-x-16">
-          {popularProducts.map((popularProduct) => {
-            return (
-              <RecipeCard key={popularProduct.Name} Product={popularProduct} />
-            );
-          })}
+          {isLoading
+            ? Array.from({ length: 4 }).map((_, index) => (
+                <RecipeCardLoader key={index} />
+              ))
+            : popularRecipes.map((recipe, index) => (
+                <RecipeCard key={index} data={recipe} />
+              ))}
         </div>
       </section>
     </>
   );
 }
 
-export default PopularProduct;
+export default PopularRecipes;
