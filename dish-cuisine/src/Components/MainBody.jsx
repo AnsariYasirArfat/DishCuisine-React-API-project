6cc0614eb5357850d18cba3ebfb9d67bdcd062d2
AnsariYasirArfat <ansariyaseer786@gmail.com>
2023-07-04 15:31:21 +0530

    filtered data & State update handled for searched functionalities

--- dish-cuisine/src/Components/MainBody.jsx
+++ dish-cuisine/src/Components/MainBody.jsx
@@ -10,10 +10,11 @@ import RecipeBook from "../Assets/RecipeBook.jpg";
 
 function MainBody() {
   const [Categories, setCategories] = useState([]);
+
   const [input, setInput] = useState("");
   const [searchedRecipe, setSearchedRecipe] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
-
+  const [userSearched, setUserSearched] = useState("");
   useEffect(() => {
     window.scrollTo(0, 0);
     async function getCategoryOfRecipeList() {
@@ -32,6 +33,26 @@ function MainBody() {
         );
         console.log(filteredCategories);
         setCategories(filteredCategories);
+        setIsLoading(true);
+
+        const List = await axios.get(
+          `https://www.themealdb.com/api/json/v1/1/search.php?s=cakes`
+        );
+        console;
+        const defualtList = List.data.meals.filter(
+          (recipe) =>
+            recipe.strCategory !== "Pork" &&
+            recipe.strCategory !== "Beef" &&
+            !recipe.strMeal.includes("Pork") &&
+            !recipe.strMeal.includes("Beef")
+        );
+        setSearchedRecipe(defualtList);
+        console.log("Default meal", defualtList);
+
+        const timeout = setTimeout(() => {
+          setIsLoading(false);
+        }, 1000);
+        return () => clearTimeout(timeout);
       } catch (error) {
         console.error("Error fetching recipes:", error);
       }
@@ -40,19 +61,43 @@ function MainBody() {
   }, []);
 
   const handleSubmit = async (event) => {
-    setIsLoading(true);
-
     event.preventDefault();
-    const searchedList = await axios.get(
-      `https://www.themealdb.com/api/json/v1/1/search.php?s=${input}`
-    );
-    console.log("search meal", searchedList.data.meals);
-    setSearchedRecipe(searchedList.data.meals);
-    const timeout = setTimeout(() => {
+    setIsLoading(true);
+    setUserSearched(input);
+    try {
+      const response = await axios.get(
+        `https://www.themealdb.com/api/json/v1/1/search.php?s=${input}`
+      );
+
+      if (response.data.meals) {
+        const filteredList = response.data.meals.filter((recipe) => {
+          const forbiddenCategories = ["Pork", "Beef"];
+          const forbiddenKeywords = ["Pork", "Beef"];
+
+          const hasForbiddenCategory = forbiddenCategories.some((category) =>
+            recipe.strCategory.includes(category)
+          );
+
+          const hasForbiddenKeyword = forbiddenKeywords.some((keyword) =>
+            recipe.strMeal.includes(keyword)
+          );
+
+          return !hasForbiddenCategory && !hasForbiddenKeyword;
+        });
+
+        setSearchedRecipe(filteredList);
+        setInput("");
+      } else {
+        setSearchedRecipe([]);
+      }
+    } catch (error) {
+      console.error("Error fetching recipes:", error);
+      // Handle the error, e.g., display an error message to the user
+    } finally {
       setIsLoading(false);
-    }, 1000);
-    return () => clearTimeout(timeout);
+    }
   };
+
   return (
     <>
       {/* Banners' Section */}
@@ -66,7 +111,7 @@ function MainBody() {
             src={Banner}
             alt=""
           />
-          <figcaption className="py-4 px-6 absolute bottom-4 left-2/4 -translate-x-2/4 w-11/12  md:w-2/4 md:h-64 lg:h-96 md:right-14 md:bottom-2/4 md:translate-y-2/4 md:-translate-x-0 md:left-auto  flex  justify-between rounded-xl bg-green-500 bg-opacity-40 hover:bg-opacity-50 cursor-pointer">
+          <figcaption className="py-4 px-6 absolute bottom-4 left-2/4 -translate-x-2/4 w-11/12  md:w-2/4 md:h-64 lg:h-96 md:right-14 md:bottom-2/4 md:translate-y-2/4 md:-translate-x-0 md:left-auto  flex  justify-between rounded-xl bg-green-500 bg-opacity-40 cursor-pointer">
             <div className="my-auto">
               <Typography className="text-lg sm:text-2xl md:text-3xl lg:text-4xl xl:text-5xl 2xl:text-6xl font-bold text-green-50">
                 Discover Culinary Excellence with Dish Cuisine Recipes!
@@ -109,24 +154,25 @@ function MainBody() {
         </div>
       </section>
       {/* Search Recipes Section */}
-      <section className="py-4 px-2 sm:px-5 lg:px-4" id="searchrecipe">
+      <section className="py-4 px-2 sm:px-5 lg:px-4 " id="searchrecipe">
         <h2 className="pb-5 lg:pb-10 text-xl sm:text-2xl xl:text-3xl 2xl:text-4xl font-bold text-center text-teal-500">
           Search your desired Meal:
         </h2>
-        <div className="w-96 h-20 mx-auto">
-          <form onSubmit={handleSubmit} className="flex">
+        <div className="w-[340px] sm:w-96 h-20 mx-auto">
+          <form onSubmit={handleSubmit} className="flex justify-between ">
             <Input
               color="teal"
               label="Search Your Meals"
               value={input}
+              size="lg"
               onChange={(e) => setInput(e.target.value)}
-              className="h-12"
+              className="capitalize "
             />
             <Button
               type="submit"
               variant="contained"
               color="teal"
-              className="h-12"
+              className="h-11 ms-2"
             >
               <img
                 width="30"
@@ -137,13 +183,37 @@ function MainBody() {
             </Button>
           </form>
         </div>
+        <div>
+          {searchedRecipe.length > 0 && userSearched && (
+            <h1 className="text-xl md:text-2xl lg:text-3xl 2xl:text-4xl font-bold text-center p-8 text-teal-900 capitalize">
+              Discover {searchedRecipe.length} Recipes: Matching your search for
+              &quot;<span className="text-teal-300">{userSearched}</span>&quot;
+            </h1>
+          )}
+        </div>
+
         <div
-          className="overflow-auto pt-0 pb-4 grid justify-items-center gap-6 grid-cols-2 sm:grid-cols-3 sm:gap-y-10 md:grid-cols-4 xl:grid-cols-5 justify-center"
-          style={{ minHeight: "600px" }}
+          className={
+            searchedRecipe.length > 0 &&
+            `overflow-auto pt-0 pb-4 grid justify-items-center gap-6 grid-cols-2 sm:grid-cols-3 sm:gap-y-10 md:grid-cols-4 xl:grid-cols-5 justify-center`
+          }
+          style={{ minHeight: "300px" }}
         >
-          {searchedRecipe.map((recipe, index) => {
-            return <RecipeCard key={index} data={recipe} Loading={isLoading} />;
-          })}
+          {searchedRecipe && searchedRecipe.length > 0 ? (
+            searchedRecipe.map((recipe, index) => (
+              <RecipeCard key={index} data={recipe} Loading={isLoading} />
+            ))
+          ) : (
+            <p className="px-10 text-center text-base md:text-lg lg:text-2xl font-bold mt-24 lg:mt-32 text-teal-950">
+              Oops! We couldn&apos;t find any recipes matching your search for
+              &quot;
+              <span className=" text-teal-500 font-extrabold ">
+                {userSearched}
+              </span>
+              &quot; at the moment. Please try a different search term or
+              explore our other delightful recipes.
+            </p>
+          )}
         </div>
       </section>
       <RandomRecipes />
