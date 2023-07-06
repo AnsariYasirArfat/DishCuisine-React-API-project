c9720a47da5a405fe3e5b8b539d5a8fc1c8562e7
AnsariYasirArfat <ansariyaseer786@gmail.com>
2023-07-06 21:47:59 +0530

    Context Api created to form favoriteRecipes array

--- dish-cuisine/src/main.jsx
+++ dish-cuisine/src/main.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { createContext, useState, useEffect } from "react";
 import * as ReactDOM from "react-dom/client";
 import "./index.css";
 import { ThemeProvider } from "@material-tailwind/react";
@@ -9,28 +9,66 @@ import {
   Route,
 } from "react-router-dom";
 import App from "./App.jsx";
-
 import MainBody from "./Components/MainBody";
 import CategoryList from "./Components/CategoryList";
 import RecipePage from "./Components/RecipePage";
 import CountryList from "./Components/CountryList";
+import FavouriteRecipes from "./Components/FavouriteRecipes";
+
+// Create a context for managing favorite recipes
+export const FavoriteRecipesContext = createContext({
+  favoriteRecipes: [],
+  updatedFavoriteRecipes: () => {},
+});
 
 const MainSection = createBrowserRouter(
   createRoutesFromElements(
     <>
       <Route path="/" element={<App />}>
         <Route index element={<MainBody />} />
-        <Route path="category" element={<CategoryList />} />
-        <Route path="area" element={<CountryList />} />
-        <Route path="recipepage" element={<RecipePage />} />
+        <Route path="/category" element={<CategoryList />} />
+        <Route path="/area" element={<CountryList />} />
+        <Route path="/recipepage" element={<RecipePage />} />
+        <Route path="/favourite" element={<FavouriteRecipes />} />
       </Route>
     </>
   )
 );
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <ThemeProvider>
-      <RouterProvider router={MainSection} />
-    </ThemeProvider>
-  </React.StrictMode>
-);
+
+const Root = () => {
+  // Getting the data from localStorage
+  let favoriteList;
+  if (localStorage.getItem("favRecipes") === null) {
+    favoriteList = [];
+  } else {
+    favoriteList = JSON.parse(localStorage.getItem("favRecipes"));
+  }
+  const [favoriteRecipes, setFavoriteRecipes] = useState(favoriteList);
+
+  const updatedFavoriteRecipes = (recipes) => {
+    setFavoriteRecipes(recipes);
+  };
+
+  console.log("favorite main", favoriteRecipes);
+  // To store the data in string format in local storage
+  useEffect(() => {
+    localStorage.setItem("favRecipes", JSON.stringify(favoriteRecipes));
+  }, [favoriteRecipes]);
+
+  return (
+    <React.StrictMode>
+      {/* Provide the favorite recipes context */}
+      <FavoriteRecipesContext.Provider
+        value={{ favoriteRecipes, updatedFavoriteRecipes }}
+      >
+        <ThemeProvider>
+          <RouterProvider router={MainSection}>
+            <App />
+          </RouterProvider>
+        </ThemeProvider>
+      </FavoriteRecipesContext.Provider>
+    </React.StrictMode>
+  );
+};
+
+ReactDOM.createRoot(document.getElementById("root")).render(<Root />);
